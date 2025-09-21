40ff4d33bf0da483ef0642e72d65afeeadc00a6b
geosfeir <92866074+geosfeir@users.noreply.github.com>
2025-09-21 14:48:52 -0400

    synchronous imem

--- hw/imem.sv
+++ hw/imem.sv
@@ -1,24 +1,39 @@
 //creating instruction memory (where we put our file with the things we want to run)
 
 module imem
-#(parameter int WIDTH = 32, parameter int DEPTH = 32)
+#(parameter int WIDTH = 32, parameter int DEPTH = 65536, parameter string INIT_FILE = "../init/imem_initialization.hex")
 (
-    input logic [WIDTH-1:0] iaddr, //instruction address aka program counter
-    // add read enable?
+    input logic clk,
+    input logic rst,
+    input logic rd_en,
+    input logic [31:0] iaddr, //instruction address aka program counter
     output logic [WIDTH-1:0] instr //instruction
 );
 
-logic [WIDTH-3:0] address = '0;
-logic [WIDTH-1:0] memory [0:DEPTH-1] = '0;
+logic [WIDTH-1:0] memory [0:DEPTH-1];
 
-always_comb begin
-    if (iaddr[7:0] != 0) begin
-        instr = '0;
-        $display("ERROR: Misaligned instruction memory address.")
-    end else begin
-        address = (iaddr >> 2);
-        instr = memory[address];
+logic [$clog2(DEPTH)-1:0] iaddr_r;  //16 bit address register
+logic [WIDTH-1:0] instr_r;
+
+initial begin
+    if (INIT_FILE != "") $readmemh(INIT_FILE, memory);
+end
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+        iaddr_r <= '0;
+        instr_r <= '0;
+    end else if (rd_en) begin
+        if (iaddr_r[1:0] != 2'b00) begin
+            instr_r <= '0;
+            $display("ERROR: Misaligned instruction memory address.");
+        end else begin
+            iaddr_r <= iaddr[31:2];
+            instr_r <= memory[iaddr_r];
+        end
     end
 end
+
+assign instr = instr_r;
 
 endmodule
